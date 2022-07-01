c6868788cfbe9ff246c0f833c1e6cab83574fbcb
Mysteriou85 <guib5@hotmail.fr>
2022-07-01 17:26:53 +0200

    tp-cycle (reprise de la correction) 3

--- tp-cycles/src/main/java/fr/diginamic/bll/exception/APiedCestBienAussiException.java
+++ tp-cycles/src/main/java/fr/diginamic/bll/exception/APiedCestBienAussiException.java
@@ -1,7 +1,7 @@
 package fr.diginamic.bll.exception;
 
-public class CycleIndisponibleException extends Exception {
-    public CycleIndisponibleException(String message) {
+public class APiedCestBienAussiException extends Exception {
+    public APiedCestBienAussiException(String message) {
         super(message);
     }
 }
--- tp-cycles/src/main/java/fr/diginamic/bll/exception/CycleIndisponibleException.java
+++ tp-cycles/src/main/java/fr/diginamic/bll/exception/CycleIndisponibleException.java
@@ -1,7 +1,7 @@
 package fr.diginamic.bll.exception;
 
-public class APiedCestBienAussiException extends Exception {
-    public APiedCestBienAussiException(String message) {
+public class CycleIndisponibleException extends Exception {
+    public CycleIndisponibleException(String message) {
         super(message);
     }
 }
