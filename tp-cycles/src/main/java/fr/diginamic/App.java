916592aad1c7ce66ff9483761de18b0a9ca53793
Mysteriou85 <guib5@hotmail.fr>
2022-07-01 17:29:39 +0200

    correction

--- tp-cycles/src/main/java/fr/diginamic/App.java
+++ tp-cycles/src/main/java/fr/diginamic/App.java
@@ -1,16 +1,10 @@
 package fr.diginamic;
 
-import fr.diginamic.bo.entity.cycle.Cycle;
-import fr.diginamic.bo.entity.cycle.Gyropode;
-import fr.diginamic.bo.entity.cycle.Gyroroue;
-import fr.diginamic.bo.entity.cycle.Velo;
 import fr.diginamic.dal.CycleDAO;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.time.LocalDate;
-import java.time.Month;
 
 public class App {
     public static void main(String[] args) {
